1e2d8a5c893ac4f974587a7aa25a9412441b9009
Myles Moylan <moylanm@fastmail.com>
2024-10-14 14:34:17 -0700

    Implement spreadGossip

--- 01_08b/main.go
+++ 01_08b/main.go
@@ -41,15 +41,25 @@ func (f *Friends) getRandomFriend() Friend {
 }
 
 // spreadGossip ensures that all the friends in the map have heard the news
-func spreadGossip(root Friend, friends Friends) {
-	panic("NOT IMPLEMENTED")
+func spreadGossip(root Friend, friends Friends, visited map[string]struct{}) {
+	for _, id := range root.Friends {
+		if _, ok := visited[id]; !ok {
+			friend := friends.getFriend(id)
+			friend.hearGossip()
+			visited[id] = struct{}{}
+
+			spreadGossip(friend, friends, visited)
+		}
+	}
 }
 
 func main() {
 	friends := importData()
 	root := friends.getRandomFriend()
 	root.hearGossip()
-	spreadGossip(root, friends)
+	visited := make(map[string]struct{})
+	visited[root.ID] = struct{}{}
+	spreadGossip(root, friends, visited)
 }
 
 // importData reads the input data from file and
