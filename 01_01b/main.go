f8a56f849c2d9bc71084540d70d7fa22dd220d86
Myles Moylan <moylanm@fastmail.com>
2024-10-13 19:13:52 -0700

    Implement parseTime

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -10,7 +10,11 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-	panic("NOT IMPLEMENTED")
+	t, err := time.Parse(expectedFormat, target)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
 }
 
 // calcSleeps returns the number of sleeps until the target.
