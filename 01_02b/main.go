3f9c2eb64ef808dd9abcdcef9611998ff1fbfe4b
Myles Moylan <moylanm@fastmail.com>
2024-10-13 20:45:09 -0700

    Implement slowDown function

--- 01_02b/main.go
+++ 01_02b/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"time"
+	"strings"
 )
 
 const delay = 700 * time.Millisecond
@@ -16,7 +17,18 @@ func print(msg string) {
 // slowDown takes the given string and repeats its characters
 // according to their index in the string.
 func slowDown(msg string) {
-	panic("NOT IMPLEMENTED")
+	words := strings.Split(msg, " ")
+
+	for _, word := range words {
+		wordLength := len(word)
+		letters := make([]string, (wordLength * (wordLength + 1)) / 2)
+
+		for i := 0; i < wordLength; i++ {
+			letters = append(letters, strings.Repeat(string(word[i]), i + 1))
+		}
+
+		print(strings.Join(letters, ""))
+	}
 }
 
 func main() {
