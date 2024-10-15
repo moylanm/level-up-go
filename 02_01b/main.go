9c73957baa837e23fd8e9b39e8029098373048e8
Myles Moylan <moylanm@fastmail.com>
2024-10-15 10:07:18 -0700

    Implement repeat function

--- 02_01b/main.go
+++ 02_01b/main.go
@@ -15,7 +15,17 @@ var messages = []string{
 
 // repeat concurrently prints out the given message n times
 func repeat(n int, message string) {
-	panic("NOT IMPLEMENTED")
+	ch := make(chan struct{})
+	for i := range n {
+		go func(i int) {
+			log.Printf("[G%d]:%s\n", i, message)
+			ch <- struct{}{}
+		}(i)
+	}
+
+	for range n {
+		<-ch
+	}
 }
 
 func main() {
